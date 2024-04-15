5fb49b2b02cd858fc8021ecdd562cfc4ce4be4ba
droschihin <dima2016123@yandex.ru>
2024-04-16 00:05:15 +0300

    1.2.Task has been fixed

--- src/Operations.java
+++ src/Operations.java
@@ -10,7 +10,23 @@ public class Operations {
 
         double bol = (double) a / (double) b;
 
-        System.out.println("a=" + a);
+        boolean crat = false;
+        if (a % 5 == 0) {
+            System.out.println("a=" + a);
+            crat = true;
+        }
+        if (b % 5 == 0) {
+            System.out.println("b=" + a);
+            crat = true;
+        }
+        if (c % 5 == 0) {
+            System.out.println("c=" + a);
+            crat = true;
+        }
+        if(!crat){
+            System.out.println("нет значений, кратных 5");
+        }
+
         System.out.println("Результат целочисленного деления a на b: " + a / b);
         System.out.printf("Результат деления a на b: %.12f %n", bol);
         System.out.println("Результат деления a на b с округлением в большую сторону: " + (int) Math.ceil(bol));
