8b8d94a369814172e4f07f8292d9855009bd3efc
droschihin <dima2016123@yandex.ru>
2024-04-16 00:37:07 +0300

    2.1.Classes&Objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите имя первого пользователя");
+        String userName1 = in.nextLine();
+        System.out.println("Введите возраст первого пользователя");
+        int userAge1 = in.nextInt();
+        in.nextLine();
+        User user1 = new User(userName1, userAge1);
+
+        System.out.println("Введите имя второго пользователя");
+        String userName2 = in.nextLine();
+        System.out.println("Введите возраст второго пользователя");
+        int userAge2 = in.nextInt();
+        in.nextLine();
+        User user2 = new User(userName2, userAge2);
+
+        if (user1.getAge() < user2.getAge()) {
+            System.out.println(user1.toString());
+        } else {
+            System.out.println(user2.toString());
+        }
+
     }
 }
