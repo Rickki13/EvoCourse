040a074d985ce819efd4e63c769ff4340504a4b3
droschihin <dima2016123@yandex.ru>
2024-05-22 19:34:45 +0300

    4.3.Location

--- location/src/main/java/com/example/location/controller/LocationController.java
+++ location/src/main/java/com/example/location/controller/LocationController.java
@@ -1,6 +1,8 @@
 package com.example.location.controller;
 
+import com.example.location.model.Geodata;
 import com.example.location.model.Location;
+import com.example.location.repository.GeodataRepository;
 import com.example.location.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,22 +15,34 @@ import java.util.Optional;
 @RequestMapping("/location")
 public class LocationController {
     @Autowired
-    private LocationRepository repository;
+    private GeodataRepository repository;
 
     @GetMapping
-    public Iterable<Location> findAll() {
-        return repository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Location> findById(@PathVariable int id) {
-        return repository.findById(id);
+    public Optional<Geodata> getLocation(@RequestParam String name) {
+        return repository.findByName(name);
     }
 
     @PostMapping
-    public ResponseEntity<Location> save(@RequestBody Location location) {
-        return repository.findById(location.getId()).isPresent()
-                ? new ResponseEntity(repository.findById(location.getId()), HttpStatus.BAD_REQUEST)
-                : new ResponseEntity(repository.save(location), HttpStatus.CREATED);
+    public Geodata save(@RequestBody Geodata geodata) {
+        return repository.save(geodata);
     }
+//    @Autowired
+//    private LocationRepository repository;
+//
+//    @GetMapping
+//    public Iterable<Location> findAll() {
+//        return repository.findAll();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Optional<Location> findById(@PathVariable int id) {
+//        return repository.findById(id);
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<Location> save(@RequestBody Location location) {
+//        return repository.findById(location.getId()).isPresent()
+//                ? new ResponseEntity(repository.findById(location.getId()), HttpStatus.BAD_REQUEST)
+//                : new ResponseEntity(repository.save(location), HttpStatus.CREATED);
+//    }
 }
